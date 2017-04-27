86591b52bf78d97528103b6c324bf237d7086353
Ruslan <breitovo76@gmail.com>
2017-04-27 17:47:15 +0300

    add class JFX_1

--- Core_JavaFX/src/core_JavaFX/Vladislav_Kavunenko/JFX_1.java
+++ Core_JavaFX/src/core_JavaFX/Vladislav_Kavunenko/JFX_1.java
@@ -3,13 +3,19 @@ package core_JavaFX.Vladislav_Kavunenko;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class JFX_1 extends Application{
+public class JFX_1 extends Application {
 
 	@Override
-	public void start(Stage arg0) throws Exception {
-		// TODO Auto-generated method stub
+	public void start(Stage primaryStage) throws Exception {  
+		primaryStage.setTitle("Окно приложения");// название заголовка окна
 		
-	} 
-	
+		 
+		
+		primaryStage.show();// показываем окно
+	}
+
+	public static void main(String[] args) {
+
+	}
 
 }
