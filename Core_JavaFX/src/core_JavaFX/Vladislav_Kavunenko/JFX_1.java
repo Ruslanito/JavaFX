2fe396e0a4889e51741f78328ab2b349e5949f7f
Ruslan <breitovo76@gmail.com>
2017-04-27 18:14:46 +0300

    add package

--- Core_JavaFX/src/core_JavaFX/Vladislav_Kavunenko/JFX_1.java
+++ Core_JavaFX/src/core_JavaFX/Vladislav_Kavunenko/JFX_1.java
@@ -1,16 +1,24 @@
 package core_JavaFX.Vladislav_Kavunenko;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class JFX_1 extends Application {
 
 	@Override
-	public void start(Stage primaryStage) throws Exception {  
+	public void start(Stage primaryStage) throws Exception {
 		primaryStage.setTitle("Окно приложения");// название заголовка окна
-		
-		 
-		
+		primaryStage.setWidth(300);// задаём ширину окна
+		primaryStage.setHeight(300); // задаём высоту окна
+		primaryStage.setResizable(true);// можно изменять размер
+		primaryStage.setOpacity(50);// прозрачность
+
+		Pane root = new Pane();
+
+		Scene scene1 = new Scene(root); // создаём сцену
+
 		primaryStage.show();// показываем окно
 	}
 
