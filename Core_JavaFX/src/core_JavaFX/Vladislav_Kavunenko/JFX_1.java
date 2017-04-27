767078a46a8d76a53a259b44cc257b36fe2e0ac6
Ruslan <breitovo76@gmail.com>
2017-04-27 17:41:03 +0300

    add package Core_JavaFX

---
+++ Core_JavaFX/src/core_JavaFX/Vladislav_Kavunenko/JFX_1.java
@@ -0,0 +1,15 @@
+package core_JavaFX.Vladislav_Kavunenko;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class JFX_1 extends Application{
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	} 
+	
+
+}
