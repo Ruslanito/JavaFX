2f540ecd2e419e8d0c2d76ef573489f6e5eaa3af
Ruslanito <breitovo76@gmail.com>
2017-04-29 22:34:28 +0300

    add code

--- Core_JavaFX/src/core_JavaFX/Vladislav_Kavunenko/L1_Button/JFX2_BorderPane.java
+++ Core_JavaFX/src/core_JavaFX/Vladislav_Kavunenko/L1_Button/JFX2_BorderPane.java
@@ -1,6 +1,7 @@
 package core_JavaFX.Vladislav_Kavunenko.L1_Button;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
@@ -14,7 +15,8 @@ public class JFX2_BorderPane extends Application{
 		BorderPane root =new BorderPane(); //выбираем основную панель
 		VBox vbox = new VBox(10);//выбираем вертикальную панель, растояние между элементами 10
 		HBox hbox = new HBox (10);//выбираем горизонтад панель, растояние между элементами 10
-		 
+		hbox.setAlignment(Pos.CENTER);//выравниваем по центру горизонтальную часть 
+		
 		Button btn1 = addButton("#1"); 
 		Button btn2 = addButton("#2");
 		Button btn3 = addButton("#3"); 
@@ -22,10 +24,17 @@ public class JFX2_BorderPane extends Application{
 		Button btn5 = addButton("#5");
 		Button btn6 = addButton("#6");
 		Button btn7 = addButton("#7");
-		Button btn8 = addButton("#8");
+		Button btn8 = addButton("#8"); 
+		 
+		vbox.getChildren().addAll(btn1,btn2,btn3,btn4); // распределяем кнопки 
+		hbox.getChildren().addAll(btn5,btn6,btn7,btn8); // распределяем кнопки 
+		
+		root.setTop(hbox);//размещение конопк вверху
+		root.setLeft(vbox);//размещение конопк слева
+		
 		
 	} 
-	  
+	 //метод для создания кнопок 
 	private Button addButton(String name){
 		Button btn = new Button(name);
 		btn.setPrefSize(50,30);
