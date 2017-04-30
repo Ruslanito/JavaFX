47fc752a1cc121950a4a65a2d6255ba094912893
Ruslanito <breitovo76@gmail.com>
2017-04-30 22:57:26 +0300

    add code in JFX3_label

--- Core_JavaFX/src/core_JavaFX/Vladislav_Kavunenko/L1_Button/JFX3_Label.java
+++ Core_JavaFX/src/core_JavaFX/Vladislav_Kavunenko/L1_Button/JFX3_Label.java
@@ -2,6 +2,7 @@ package core_JavaFX.Vladislav_Kavunenko.L1_Button;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -15,20 +16,28 @@ public class JFX3_Label extends Application {
 	}
 
 	@Override
-	public void start(Stage primaryStage) throws Exception { 
-		Pane root = new Pane(); 
-		 
-		Label label1 = new Label(); // создаём метку с надписью
-		label1.setText("Просто надпись"); // создаём текст надписи  
-		 
-		Image image1 = new Image(getClass().getResourceAsStream("200_4.jpg")); //создаём метку с картинкой
-		ImageView img1 = new ImageView(); 
-		img1.setFitHeight(300); 
-		img1.setFitWidth(300); 
-				
+	public void start(Stage primaryStage) throws Exception {
+		Pane root = new Pane();
+		// 1.создаём метку с надписью
+		Label label1 = new Label();
+		label1.setText("Просто надпись"); // создаём текст надписи
+		// 2.создаём метку с картинкой
+		Image image1 = new Image(getClass().getResourceAsStream("200_4.jpg"));
+		ImageView img1 = new ImageView(image1);
+		img1.setFitHeight(300);//размер картинки в метке
+		img1.setFitWidth(400);//размер картинки в метке
+		Label labelImg = new Label();
+		labelImg.setGraphic(img1);// вставляем картинку в метку
+		labelImg.setTranslateX(100);
+		labelImg.setTranslateY(50);
+		// 3.создаём метку с картинкой и текстом 
+		labelImg.setText("200"); //надпись в метке    
+		labelImg.setGraphicTextGap(10); //растояние между элементами
+		labelImg.setContentDisplay(ContentDisplay.TOP); //картинка будет вверху а текст внизу
 		
-		root.getChildren().addAll(label1); // добавляем метку с надписью в root
-		Scene scene1 = new Scene(root,600,600);  
+
+		root.getChildren().addAll(label1, labelImg); // добавляем метки в root
+		Scene scene1 = new Scene(root, 600, 500);
 		primaryStage.setScene(scene1);
 		primaryStage.show();
 	}
