8d07216be05762894ff741060d924f74c1df213d
Ruslanito <breitovo76@gmail.com>
2017-04-30 22:40:20 +0300

    add class JFX3_Label

--- Core_JavaFX/src/core_JavaFX/Vladislav_Kavunenko/L1_Button/JFX3_Label.java
+++ Core_JavaFX/src/core_JavaFX/Vladislav_Kavunenko/L1_Button/JFX3_Label.java
@@ -3,6 +3,8 @@ package core_JavaFX.Vladislav_Kavunenko.L1_Button;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -16,10 +18,16 @@ public class JFX3_Label extends Application {
 	public void start(Stage primaryStage) throws Exception { 
 		Pane root = new Pane(); 
 		 
-		Label label1 = new Label(); // создаём надпись
-		label1.setText("Просто надпись"); // создаём текст надписи 
+		Label label1 = new Label(); // создаём метку с надписью
+		label1.setText("Просто надпись"); // создаём текст надписи  
+		 
+		Image image1 = new Image(getClass().getResourceAsStream("200_4.jpg")); //создаём метку с картинкой
+		ImageView img1 = new ImageView(); 
+		img1.setFitHeight(300); 
+		img1.setFitWidth(300); 
+				
 		
-		root.getChildren().addAll(label1); // добавляем надпись в root
+		root.getChildren().addAll(label1); // добавляем метку с надписью в root
 		Scene scene1 = new Scene(root,600,600);  
 		primaryStage.setScene(scene1);
 		primaryStage.show();
