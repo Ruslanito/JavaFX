d67b7b4200544f6bac8d77c618309c86338de3a7
Ruslanito <breitovo76@gmail.com>
2017-05-01 22:53:46 +0300

    add code

--- Core_JavaFX/src/core_JavaFX/Vladislav_Kavunenko/L1_Button/JFX4_ButtonAdv.java
+++ Core_JavaFX/src/core_JavaFX/Vladislav_Kavunenko/L1_Button/JFX4_ButtonAdv.java
@@ -1,10 +1,18 @@
 package core_JavaFX.Vladislav_Kavunenko.L1_Button;
 
-public class JFX4_ButtonAdv {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class JFX4_ButtonAdv extends Application{
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		launch(args);
+		}
 
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
