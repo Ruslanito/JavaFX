4d1e8da6fe6ca60290069d9ca29768eb620f4b23
Ruslanito <breitovo76@gmail.com>
2017-05-02 22:48:56 +0300

    add code in class JFX5_RB2

--- Core_JavaFX/src/core_JavaFX/Vladislav_Kavunenko/JFX5_RadioButton2.java
+++ Core_JavaFX/src/core_JavaFX/Vladislav_Kavunenko/JFX5_RadioButton2.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
@@ -19,7 +20,13 @@ public class JFX5_RadioButton2 extends Application {
 		RadioButton rb1 = new RadioButton("1");
 		RadioButton rb2 = new RadioButton("2");
 		RadioButton rb3 = new RadioButton("3");
-
+ 
+		ToggleGroup group = new ToggleGroup();//создаём группу для радио-кнопок
+		rb1.setToggleGroup(group);//добавляем кнопки в группу
+		rb2.setToggleGroup(group);//добавляем кнопки в группу
+		rb3.setToggleGroup(group);//добавляем кнопки в группу
+		rb2.setSelected(true);//указываем кнопку по-умолчанию
+		
 		BorderPane root = new BorderPane();
 		HBox buttons = new HBox(10);
 		buttons.setAlignment(Pos.CENTER);
