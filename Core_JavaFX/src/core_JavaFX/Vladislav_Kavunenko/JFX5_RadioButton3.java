7954eae28342004d3099e2c31766118405c05f64
Ruslanito <breitovo76@gmail.com>
2017-05-03 22:58:50 +0300

    add photos in package

--- Core_JavaFX/src/core_JavaFX/Vladislav_Kavunenko/JFX5_RadioButton3.java
+++ Core_JavaFX/src/core_JavaFX/Vladislav_Kavunenko/JFX5_RadioButton3.java
@@ -5,6 +5,8 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
@@ -23,24 +25,33 @@ public class JFX5_RadioButton3 extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		BorderPane root = new BorderPane();
+		HBox buttons = new HBox(10);
+		buttons.setAlignment(Pos.CENTER);
+		
+		ToggleGroup group = new ToggleGroup();//создаём группу для радио-кнопок
+		
+		
 		RadioButton rb1 = new RadioButton("1");
 		RadioButton rb2 = new RadioButton("2");
 		RadioButton rb3 = new RadioButton("3");
  
-		ToggleGroup group = new ToggleGroup();//создаём группу для радио-кнопок
+		
 		rb1.setToggleGroup(group);//добавляем кнопки в группу
 		rb2.setToggleGroup(group);//добавляем кнопки в группу
 		rb3.setToggleGroup(group);//добавляем кнопки в группу
 		rb2.setSelected(true);//указываем кнопку по-умолчанию
 		
-		BorderPane root = new BorderPane();
-		HBox buttons = new HBox(10);
-		buttons.setAlignment(Pos.CENTER);
-		buttons.getChildren().addAll(rb1,rb2,rb3);
+		rb1.setUserData("1");
+		rb2.setUserData("2");
+		rb3.setUserData("3");
 		
+		
+		
+		buttons.getChildren().addAll(rb1,rb2,rb3);
 		root.setTop(buttons);
-		Scene scene1 = new Scene(root, 500, 500);
-		primaryStage.setScene(scene1);
+		Scene scene = new Scene(root, 500, 500);
+		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
 
