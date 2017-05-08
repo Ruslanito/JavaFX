927306d3e910ee6adaad8228ba9b8fcf8bfaf3a7
Ruslanito <breitovo76@gmail.com>
2017-05-08 22:23:46 +0300

    add class JFX6_ToggleButton.java

---
+++ Core_JavaFX/src/core_JavaFX/Vladislav_Kavunenko/JFX6_ToggleButton.java
@@ -0,0 +1,18 @@
+package core_JavaFX.Vladislav_Kavunenko;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class JFX6_ToggleButton extends Application{
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
