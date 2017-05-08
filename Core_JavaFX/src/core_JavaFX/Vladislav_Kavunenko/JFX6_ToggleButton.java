505049e219eaafaa2898c08dec9d2460bd02075f
Ruslanito <breitovo76@gmail.com>
2017-05-08 22:53:41 +0300

    add code

--- Core_JavaFX/src/core_JavaFX/Vladislav_Kavunenko/JFX6_ToggleButton.java
+++ Core_JavaFX/src/core_JavaFX/Vladislav_Kavunenko/JFX6_ToggleButton.java
@@ -4,14 +4,15 @@ import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
-public class JFX6_ToggleButton extends Application{
-	
+public class JFX6_ToggleButton extends Application {
+
 	public static void main(String[] args) {
 		launch(args);
 	}
@@ -19,22 +20,46 @@ public class JFX6_ToggleButton extends Application{
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		BorderPane root = new BorderPane();
-		
-		HBox buttons = new HBox();		
+
+		HBox buttons = new HBox();
 		buttons.setAlignment(Pos.CENTER);
-		
+
+		ToggleGroup group = new ToggleGroup();
+
 		ToggleButton tb1 = new ToggleButton("1");
 		ToggleButton tb2 = new ToggleButton("2");
 		ToggleButton tb3 = new ToggleButton("3");
+
+		tb1.setToggleGroup(group);
+		tb2.setToggleGroup(group);
+		tb3.setToggleGroup(group);
 		
-		Rectangle rect = new Rectangle(300,300);
+		tb1.setUserData(Color.LIGHTGREEN);
+		tb2.setUserData(Color.LIGHTBLUE);
+		tb3.setUserData(Color.SALMON);
+		
+		Rectangle rect = new Rectangle(300, 300);
 		rect.setFill(Color.WHITE);
 		rect.setStroke(Color.DARKGOLDENROD);
+		rect.setStrokeWidth(2);
+		rect.setArcHeight(50);
+		rect.setArcWidth(50);
 		
-		buttons.getChildren().addAll(tb1,tb2,tb3);
+		group.selectedToggleProperty().addListener(event ->{
+			if(group.getSelectedToggle() !=null){
+				rect.setFill((Color)group.getSelectedToggle().getUserData());
+			}
+			else {
+				rect.setFill(Color.WHITE);
+			}
+		
+		});
+
+		buttons.getChildren().addAll(tb1, tb2, tb3);
 		root.setTop(buttons);
-		Scene scene1 = new Scene(root,500,500);
+		root.setCenter(rect);
+		Scene scene1 = new Scene(root, 500, 500);
 		primaryStage.setScene(scene1);
-		primaryStage.show();		
+		primaryStage.show();
 	}
 }
