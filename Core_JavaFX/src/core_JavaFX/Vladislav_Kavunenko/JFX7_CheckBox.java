137e9eb5c6ae9edba306b4169e5b5284d6a42fe6
Ruslanito <breitovo76@gmail.com>
2017-05-09 21:49:42 +0300

    add code

--- Core_JavaFX/src/core_JavaFX/Vladislav_Kavunenko/JFX7_CheckBox.java
+++ Core_JavaFX/src/core_JavaFX/Vladislav_Kavunenko/JFX7_CheckBox.java
@@ -1,10 +1,24 @@
 package core_JavaFX.Vladislav_Kavunenko;
 
-public class JFX7_CheckBox {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class JFX7_CheckBox extends Application {
 
 	public static void main(String[] args) {
-		
+		launch (args);
 	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+		Scene scene1 = new Scene(root,500,500);
+		primaryStage.setScene(scene1);
+		primaryStage.show();
+	}
+	
+	
 	
 	
 
