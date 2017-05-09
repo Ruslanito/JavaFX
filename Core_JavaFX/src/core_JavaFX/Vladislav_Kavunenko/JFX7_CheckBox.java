8cb2144837a4acb0af6955b0e63cd8c8f0a1d62c
Ruslanito <breitovo76@gmail.com>
2017-05-09 21:54:46 +0300

    add 1 part

--- Core_JavaFX/src/core_JavaFX/Vladislav_Kavunenko/JFX7_CheckBox.java
+++ Core_JavaFX/src/core_JavaFX/Vladislav_Kavunenko/JFX7_CheckBox.java
@@ -2,6 +2,7 @@ package core_JavaFX.Vladislav_Kavunenko;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class JFX7_CheckBox extends Application {
@@ -12,7 +13,9 @@ public class JFX7_CheckBox extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-
+		primaryStage.setTitle("Окно приложения");
+		BorderPane root = new BorderPane();
+		
 		Scene scene1 = new Scene(root,500,500);
 		primaryStage.setScene(scene1);
 		primaryStage.show();
