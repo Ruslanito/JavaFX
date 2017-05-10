673c95ce4cdcf012be64f4c483276801fd3f82d4
Ruslanito <breitovo76@gmail.com>
2017-05-10 22:10:57 +0300

    add code

--- Core_JavaFX/src/core_JavaFX/Vladislav_Kavunenko/JFX8_TextField_GridPane.java
+++ Core_JavaFX/src/core_JavaFX/Vladislav_Kavunenko/JFX8_TextField_GridPane.java
@@ -1,10 +1,13 @@
 package core_JavaFX.Vladislav_Kavunenko;
 
+import java.awt.TextField;
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
-public class JFX8_TextField_GridPane extends Application{
+public class JFX8_TextField_GridPane extends Application {
 
 	public static void main(String[] args) {
 		launch(args);
@@ -13,8 +16,25 @@ public class JFX8_TextField_GridPane extends Application{
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		GridPane root = new GridPane();
+		root.setPadding(new Insets(10, 10, 10, 10));
+		root.setHgap(5);
+		root.setVgap(5);
+
+		TextField name = new TextField("Имя");
+		GridPane.setConstraints(name, 0, 0);
+		root.getChildren().add(name);
 		
-		Scene scene1 = new Scene(root,500,500);
+		TextField lastname = new TextField("Фамилия");
+		GridPane.setConstraints(lastname, 0, 1);
+		root.getChildren().add(lastname);
+		
+		TextField comment = new TextField();
+		comment.setPromptText("Введи комментарий");
+		GridPane.setConstraints(comment, 0, 2);
+		root.getChildren().add(comment);		
+
+		Scene scene1 = new Scene(root, 500, 500);
 		primaryStage.setScene(scene1);
 		primaryStage.show();
 	}
